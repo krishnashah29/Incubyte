c1aedb6927d04977ede11fd91e0329e39473ec1d
krishnashah29 <krishnapshah252@gmail.com>
2021-08-30 06:22:37 +0530

    NumbersWithComma

--- String_Calculator_parts.py
+++ String_Calculator_parts.py
@@ -9,6 +9,12 @@ class Tests_StringCalculator(unittest.TestCase):
     def test_SingleNumber(self):
         self.assertEqual(StringCalculator.Add("1"),1)
         self.assertEqual(StringCalculator.Add("2"),2)
+    
+    #1.3
+    def test_NumbersWithComma(self):
+        self.assertEqual(StringCalculator.Add("4,5"),9)
+        self.assertEqual(StringCalculator.Add("1,2,3"),6)
+        self.assertEqual(StringCalculator.Add("1,2,3,4,5"),15)
 
 class StringCalculator():
     def Add(self):
@@ -20,5 +26,14 @@ class StringCalculator():
         if len(self)==1:
             return int(self)
 
+        #NumbersWithComma
+        if "," in self:
+            return sum(StringCalculator.ExtractNumbers(self))
+
+    
+    def ExtractNumbers(self):
+        numbers=self.split(',')
+        numbers=[int(x) for x in numbers if int(x)<=1000]
+        return numbers
 if __name__=='__main__':
     unittest.main()
