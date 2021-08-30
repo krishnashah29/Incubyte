f612e3e0ce2b97fccf27099e8a8e9402b3e042ae
krishnashah29 <krishnapshah252@gmail.com>
2021-08-30 06:18:36 +0530

    SingleNumbers

--- String_Calculator_parts.py
+++ String_Calculator_parts.py
@@ -5,11 +5,20 @@ class Tests_StringCalculator(unittest.TestCase):
     def test_EmptyString_return_0(self):
         self.assertEqual(StringCalculator.Add(""),0)
 
+     #1.1
+    def test_SingleNumber(self):
+        self.assertEqual(StringCalculator.Add("1"),1)
+        self.assertEqual(StringCalculator.Add("2"),2)
+
 class StringCalculator():
     def Add(self):
         #Empty String 
         if self=="":
             return 0
+        
+        #SingleNumbers
+        if len(self)==1:
+            return int(self)
 
 if __name__=='__main__':
     unittest.main()
