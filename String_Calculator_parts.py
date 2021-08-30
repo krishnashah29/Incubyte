71efb0b6481cb3df8d6fbff7d3c68f5309a25f20
krishnashah29 <krishnapshah252@gmail.com>
2021-08-30 06:31:37 +0530

    NegativeNumbers

--- String_Calculator_parts.py
+++ String_Calculator_parts.py
@@ -25,6 +25,12 @@ class Tests_StringCalculator(unittest.TestCase):
         self.assertEqual(StringCalculator.Add("//;\n1;2;3"),6)
         self.assertEqual(StringCalculator.Add("//##\n1##2##3##4##5"),15)
         self.assertEqual(StringCalculator.Add("//;#;\n1;#;2;#;3"),6)
+    
+    #5,6.
+    def test_ExceptionForNegativeNumbers(self):
+        self.assertRaises(Exception,StringCalculator.Add("-6"))
+        self.assertRaises(Exception,StringCalculator.Add("-15,-6"))
+        self.assertRaises(Exception,StringCalculator.Add("-15,-6,-1,-5"))
 
 class StringCalculator():
     global numbers
@@ -43,8 +49,12 @@ class StringCalculator():
             if "\n" in self:
                 return StringCalculator.NewLinesBetweenNumbers(self)
             
-            return sum(StringCalculator.ExtractNumbers(self))
-        
+            result=sum(StringCalculator.ExtractNumbers(self))
+            if result<0:
+                return StringCalculator.Error(numbers)
+            else:
+                return result
+
         #DifferentDelimiters
         if self.startswith("//"):
             return StringCalculator.MultipleDelimiters(self)
@@ -69,6 +79,13 @@ class StringCalculator():
         numbers=numbers.split(delimiters[2:])
         numbers=[int(x) for x in numbers if int(x)<=1000]
         return sum(numbers)
+    def Error(self):
+        try:
+            Nnums=[x for x in self if x<'0']
+            error_msg="Negative numbers are not allowed-"+Nnums
+            raise Exception(error_msg)
+        except Exception:
+            pass
     
     
 if __name__=='__main__':
